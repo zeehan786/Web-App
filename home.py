dd766a9995c23bb6e9398a208adb04f6e417534a
Zeehan <70279842+zeehan786@users.noreply.github.com>
2022-12-06 17:46:42 -0500

    Update home.py

--- home.py
+++ home.py
@@ -35,19 +35,21 @@ st.set_page_config(
 
 
 
-rad = st.sidebar.radio("Navigation", ["Home", "Data Visualization", "About Us"])
+rad = st.sidebar.radio("Navigation", ["Home", "Data Visualization", "Our Data", "About Us"])
 
 if rad == "Home":
     st.header("Welcome to our Airline Data Visualization Homepage! 👋")
+    st.image('images/q2.png')
+    st.subheader("For our project we will be using a dataset that is derived from the Bureau of Transportation Statistics that tracks airplane flights in the United States from 2019, 2020, 2021, and 2022. We will be visualizing how flights have changed throughout these years, the activities of domestic flights throughout the USA, and the effect of covid 19 on air travel.")
 
 elif rad == "Data Visualization":
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Visualization 1", "Visualization 2", "Visualization 3",
-                                                         "Visualization 4", "Visualization 5", "Visualization 6"])
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Visualization 1", "Visualization 2", "Visualization 3",
+                                                         "Visualization 4", "Visualization 5", "Visualization 6", "Visualization 7"])
     
     with tab1:
         st.header("Domestic Airlines trend during covid-19")
-        st.image('images/V1-Price.png')
-        
+        st.image('images/V1_4.png', width=210)
+        st.image('images/V1-Price.png')        
         st.image('images/V1-Miles.png')
     
     with tab2:
@@ -56,7 +58,7 @@ elif rad == "Data Visualization":
         st.image('images/V2-Pie.png')
 
     with tab3:
-        st.header("Which state has the highest market fare?")
+        st.header("Which state has the highest count of flight departures/arrivals?")
         HtmlFile = open("images/V3-Map.html", 'r', encoding='utf-8')
         source_code = HtmlFile.read() 
         # print(source_code)
@@ -65,16 +67,31 @@ elif rad == "Data Visualization":
     
     with tab4:
         st.header("Ideal time to book a fare?")
-        st.image('images/V4-Ideal_time.png')
+        st.image('images/V1_4.png', width=210)
+        st.image('images/V4-Ideal_time.png')        
     
     with tab5:
         st.header("Which airline company has the highest fare ticket price?")
+        st.image('images/V5_6.png', width=810)
         st.image('images/V5-Airline_price.png')
+        
     
     with tab6:
         st.header("America's Favorite Airline")
+        st.image('images/V5_6.png', width=810)
         st.image('images/V6-AmericaFav.png')
+        
     
+    with tab7:
+        st.header("Which state has the highest airline miles? (Destination/Arrival)")
+        HtmlFile2 = open("images/V7-Arr.html", 'r', encoding='utf-8')
+        source_code2 = HtmlFile2.read()
+        components.html(source_code2, height = 450)
+
+        HtmlFile3 = open("images/V7-Dep.html", 'r', encoding='utf-8')
+        source_code3 = HtmlFile3.read()
+        components.html(source_code3, height = 450)
+
 elif rad == "About Us":
     st.header("About Us")
 
@@ -97,3 +114,13 @@ elif rad == "About Us":
         st.write("Senior at Queens College")
         st.write("Computer Science Major")
         st.write('<a href="https://www.linkedin.com/in/zeehan-rahman">Linkedin</a>', unsafe_allow_html=True)
+
+elif rad == "Our Data":
+    st.header("Data")
+    st.image('images/Our_Data_df.png')
+    st.subheader("Our data holds values for domestic airline flights in the US from 2019 to 2022. The data was collected from the bureau of transportation statistics. We have about 30 million records of flight travels. Each record has unique attributes which are year and quarter. These variables help give us an insight on the time people booked their flights. ")
+    st.write('<a href="https://www.transtats.bts.gov/">Data Source</a>', unsafe_allow_html=True)
+    st.image('images/Our_Data_table.png')
+    st.subheader("There are a lot of different variables that go into air travel. With these variables, we were able to determine which one has the most significant impact on the price of an airplane ticket. Were also able to use these variables to check air travel activities throughout the years from 2019-2022. They will also help us identify the trends caused by covid 19. ")
+    st.subheader("Because of this dataset, we are able to ask so many questions and get answers in return. Feel free to explore these questions.")
+
